b3878e424ab90b800c46a5cd540809408478830e
Illia Zelenin <wyster@make.im>
2025-06-18 06:39:05 -0700

    add cache & command

---
+++ src/Command/CheckAvailabilityCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Service\WizzMultipassService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'check-availability',
+)]
+class CheckAvailabilityCommand extends Command
+{
+    public function __construct(
+        private readonly WizzMultipassService $wizzMultipass,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption('origin', null, InputOption::VALUE_REQUIRED, 'Origin')
+            ->addOption('destination', null, InputOption::VALUE_REQUIRED, 'Destination')
+            ->addOption('departure', null, InputOption::VALUE_REQUIRED, 'Departure')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $origin = (string) $input->getOption('origin');
+        if (!$origin) {
+            $output->writeln('<error>Origin is required.</error>');
+
+            return Command::FAILURE;
+        }
+        $destination = (string) $input->getOption('destination');
+        if (!$destination) {
+            $output->writeln('<error>Destination is required.</error>');
+
+            return Command::FAILURE;
+        }
+        $departure = (string) $input->getOption('departure');
+        if (!$departure) {
+            $output->writeln('<error>Departure is required.</error>');
+
+            return Command::FAILURE;
+        }
+
+        $result = $this->wizzMultipass->getAvailability(
+            $origin,
+            $destination,
+            new \DateTimeImmutable($departure)
+        );
+        dd($result);
+    }
+}
--- src/Service/WizzMultipassService.php
+++ src/Service/WizzMultipassService.php
@@ -6,33 +6,52 @@ namespace App\Service;
 
 use App\Integration\WizzMultipassIntegration;
 use App\Util\WizzMultiPassUtil;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\BrowserKit\CookieJar;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class WizzMultipassService
 {
+    private const string AUTH_CACHE_KEY = 'wizz_auth';
+
     public function __construct(
         private readonly WizzMultipassIntegration $wizzMultipass,
         #[Autowire(env: 'WIZZAIR_USERNAME')]
         private readonly string $username,
         #[Autowire(env: 'WIZZAIR_PASSWORD')]
         private readonly string $password,
+        #[Autowire(service: 'cache.app.taggable')]
+        private readonly CacheItemPoolInterface&TagAwareCacheInterface $cache,
     ) {
     }
 
     public function getAvailability(string $origin, string $destination, \DateTimeInterface $departure): array
     {
-        $cookieJar = new CookieJar();
-        $response = $this->wizzMultipass->login();
-        WizzMultiPassUtil::updateFromResponse($cookieJar, $response);
-        $action = WizzMultiPassUtil::parseAuthenticateAction($response->getContent());
-
-        $this->wizzMultipass->authenticate($this->username, $this->password, $action, $cookieJar);
+        $cookieJar = $this->authenticate();
         $wallet = $this->wizzMultipass->getWallets($cookieJar);
         WizzMultiPassUtil::updateFromResponse($cookieJar, $wallet);
         $urls = WizzMultiPassUtil::parseUrls($wallet->getContent());
         $availability = $this->wizzMultipass->getAvailability($urls['searchFlightJson'], $origin, $destination, $departure->format('Y-m-d'), $cookieJar);
 
         return $availability->toArray();
+    }
+
+    private function authenticate(): CookieJar
+    {
+        $cacheItem = $this->cache->getItem(self::AUTH_CACHE_KEY);
+        if ($cacheItem->isHit()) {
+            return $cacheItem->get();
+        }
+        $cookieJar = new CookieJar();
+        $response = $this->wizzMultipass->login();
+        WizzMultiPassUtil::updateFromResponse($cookieJar, $response);
+        $action = WizzMultiPassUtil::parseAuthenticateAction($response->getContent());
+        $this->wizzMultipass->authenticate($this->username, $this->password, $action, $cookieJar);
+
+        $cacheItem->set($cookieJar);
+        $this->cache->save($cacheItem);
+
+        return $cookieJar;
     }
 }
