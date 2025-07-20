c50a2eabd1facd3c2419f6201b65cf5f97b5cec7
Illia Zelenin <wyster@make.im>
2025-07-20 06:25:39 -0700

    table output

--- src/Command/CheckAvailabilityCommand.php
+++ src/Command/CheckAvailabilityCommand.php
@@ -9,7 +9,6 @@ use App\Service\WizzMultipassService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -84,7 +83,7 @@ class CheckAvailabilityCommand extends Command
                 $table->addRow([
                     $originCode,
                     $destinationCode,
-                    'Route is not available'
+                    'Route is not available',
                 ]);
             }
         }
